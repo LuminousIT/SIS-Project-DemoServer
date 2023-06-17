cf0cd5fbb3f52ff5085aef6ab95042f127f72cc5
Olumide Oluwole <oluwoleit@gmail.com>
2023-06-17 16:48:27 +0200

    auth headers

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,22 @@ const admin_route_handler = require("./admin/index");
 const bookings_route_handler = require("./bookings/index");
 const facility_route_handler = require("./facility/index");
 
+router.use(setup_request);
 router.use("/api/v1/admin", admin_route_handler);
 router.use("/api/v1/bookings", bookings_route_handler);
 router.use("/api/v1/facility", facility_route_handler);
 
 module.exports = router;
+
+const setup_request = (request, response, next) => {
+  request.headers["access-control-allow-origin"] = "*";
+  request.headers["access-control-allow-headers"] = "*";
+
+  if (request.method === "OPTIONS") {
+    request.headers["access-control-allow-methods"] =
+      "GET, POST, PUT, PATCH, DELETE";
+    response.status(200).json();
+  }
+
+  next();
+};
